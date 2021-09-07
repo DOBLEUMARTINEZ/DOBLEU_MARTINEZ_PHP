b712e11fbaaa7f63f50b85551ff1b912bc3207b7
JOSE MARTINEZ <jatemiz@atsonline.com.mx>
2021-09-06 22:55:58 -0500

    SE CREARON Y SE AÑADIERON LOS PRIMEROS ARCHIVOS

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cursos | Aprende git desde cero </title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+	<nav class="header">
+		<div>
+			logo
+		</div>
+		<ul>
+			<li>home</li>
+			<li>us</li>
+			<li>services</li>
+			<li>blog</li>
+			<li>contact</li>
+		</ul>
+	</nav>
+
+	<div class="body">
+		<h1>Primeros pasos en git</h1>
+	</div>
+
+	<footer class="footer">
+		<ul>
+			<li>sitemap</li>
+			<li>social media</li>
+			<li>contact</li>
+		</ul>
+	</footer>
+
+</body>
+</html>
