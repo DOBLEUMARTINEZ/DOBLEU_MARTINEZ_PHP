0c6cd21a59c111e6042b30af0777ee5da0d76da0
JOSE MARTINEZ <jatemiz@atsonline.com.mx>
2021-09-08 23:33:54 -0500

    SE CREO LA ESTRUCTURA BASICA DEL SITIO CON ALGUNOS ESTILOS

--- index.php
+++ index.php
@@ -10,38 +10,11 @@
 
 		include('includes/header.php'); // header
 
+		include('includes/home.php'); // section home
+
+		include('includes/footer.php'); // footer
+
 	?>
-
-	<section class="body">
-		<h1>PRIMEROS PASOS EN GIT</h1>
-		<br>
-		<h2>Que es GIT</h2>
-		<p>
-			Git es un software de control de versiones diseñado por Linus Torvalds, pensando en la eficiencia, la confiabilidad y compatibilidad del mantenimiento de versiones de aplicaciones cuando estas tienen un gran número de archivos de código fuente.
-		</p>
-		<br>
-		<h2>Comandos basicos de Git</h2>
-		<ol>
-			<li>git init --> inicia un nuevo repositorio de git</li>
-			<li>git status --> Nos deja ver el estatus del proyecto</li>
-			<li>git add "file name" --> Nos permite añadir un archivo a nuetro repositorio</li>
-			<li>git add -A --> Nos Permite añadir todos los archivos a nuestro repositorio</li>
-			<li>git commit -m "desc cambio" --> Nos permite añadir un nuevo cambio al repositorio</li>
-			<li>git log --> Nos permite ver todos los commit del repositorio</li>
-			<li>git checkout --> Nos permite regrfesar a cualquier commit del repositorio</li>
-			<li>git checkout master --> Nos permite regresar al ultimo commit</li>
-			<li>git reset --soft --> Nos permite borrar commits sin consecuencia</li>
-			<li>git reset --hard --> Nos permite borrar commits con consecuencias(borra todos los cambios realizados)</li>
-		</ol>
-	</section>
-
-	<footer class="footer">
-		<ul>
-			<li>sitemap</li>
-			<li>social media</li>
-			<li>contact</li>
-		</ul>
-	</footer>
 
 </body>
 </html>
